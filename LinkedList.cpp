88fa41ac76f6f2c99a8fb5425d44c95a3f154026
Nafa06 <naufalanggardi09@gmail.com>
2025-05-08 11:42:44 +0700

    menambahkan case 3

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -134,24 +134,29 @@ int main()
         mhs.addNode();
     }
     break;
-     case '2': 
+    case '2':
+    {
+        if (mhs.listEmpty())
         {
-            if (mhs.listEmpty())
-            {
-                cout << endl
-                     << "List Kosong" << endl;
-                break;
-            }
             cout << endl
-                 << "\nMasukkan no mahasiswa yang akan dihapus: ";
-            cin >> nim;
-            if (mhs.delNode(nim) == false)
-                cout << endl
-                     << "Data tidak ditemukan" << endl;
-            else
-                cout << endl
-                     << "Data dengan nomor mahasiswa " << nim << " berhasil dihapus " << endl;
+                 << "List Kosong" << endl;
+            break;
         }
-        break;
+        cout << endl
+             << "\nMasukkan no mahasiswa yang akan dihapus: ";
+        cin >> nim;
+        if (mhs.delNode(nim) == false)
+            cout << endl
+                 << "Data tidak ditemukan" << endl;
+        else
+            cout << endl
+                 << "Data dengan nomor mahasiswa " << nim << " berhasil dihapus " << endl;
+    }
+    break;
+    case '3': 
+    {
+        mhs.traverse();
+    }
+    break;
     }
 }
