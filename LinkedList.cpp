8afdb454c6c5662de5b0e709fd4f130c49159174
Nafa06 <naufalanggardi09@gmail.com>
2025-05-08 11:45:20 +0700

    Memperbaiki typo

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -126,70 +126,71 @@ int main()
         cout << endl
              << "Masukkan pilihan (1-5): ";
         cin >> ch;
-    }
-    switch (ch)
-    {
-    case '1':
-    {
-        mhs.addNode();
-    }
-    break;
-    case '2':
-    {
-        if (mhs.listEmpty())
+        switch (ch)
         {
-            cout << endl
-                 << "List Kosong" << endl;
-            break;
-        }
-        cout << endl
-             << "\nMasukkan no mahasiswa yang akan dihapus: ";
-        cin >> nim;
-        if (mhs.delNode(nim) == false)
-            cout << endl
-                 << "Data tidak ditemukan" << endl;
-        else
-            cout << endl
-                 << "Data dengan nomor mahasiswa " << nim << " berhasil dihapus " << endl;
-    }
-    break;
-    case '3':
-    {
-        mhs.traverse();
-    }
-    break;
-    case '4':
-    {
-        if (mhs.listEmpty() == true)
+        case '1':
         {
-            cout << "\nList Kosong\n";
-            break;
+            mhs.addNode();
         }
-        Node *previous, *current;
-        cout << endl
-             << "Masukkan no mahasiswa yang dicari: ";
-        cin >> nim;
-        if (mhs.Search(nim, &previous, &current) == false)
-            cout << endl
-                 << "Data tidak ditemukan" << endl;
-        else
+        break;
+        case '2':
         {
+            if (mhs.listEmpty())
+            {
+                cout << endl
+                     << "List Kosong" << endl;
+                break;
+            }
             cout << endl
-                 << "Data ditemukan" << endl;
-            cout << "\nNo Mahasiswa: " << current->noMhs << endl;
-            cout << "\n";
+                 << "\nMasukkan no mahasiswa yang akan dihapus: ";
+            cin >> nim;
+            if (mhs.delNode(nim) == false)
+                cout << endl
+                     << "Data tidak ditemukan" << endl;
+            else
+                cout << endl
+                     << "Data dengan nomor mahasiswa " << nim << " berhasil dihapus " << endl;
+        }
+        break;
+        case '3':
+        {
+            mhs.traverse();
+        }
+        break;
+        case '4':
+        {
+            if (mhs.listEmpty() == true)
+            {
+                cout << "\nList Kosong\n";
+                break;
+            }
+            Node *previous, *current;
+            cout << endl
+                 << "Masukkan no mahasiswa yang dicari: ";
+            cin >> nim;
+            if (mhs.Search(nim, &previous, &current) == false)
+                cout << endl
+                     << "Data tidak ditemukan" << endl;
+            else
+            {
+                cout << endl
+                     << "Data ditemukan" << endl;
+                cout << "\nNo Mahasiswa: " << current->noMhs << endl;
+                cout << "\n";
+            }
+        }
+        break;
+        case '5':
+        {
+            exit(0);
+        }
+        break;
+        default:
+        {
+            cout << "Pilihan salah !." << endl;
+        }
+        break;
         }
     }
-    break;
-    case '5': 
-    {
-        exit(0);
-    }
-    break;
-    default:
-    {
-        cout << "Pilihan salah !." << endl;
-    }
-    break;
-    }
+    while (ch != '5');
 }
