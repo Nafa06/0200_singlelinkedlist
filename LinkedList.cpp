0dbbdc0db6ff9ff71d8ee1df007f3ac4f8135f05
Nafa06 <naufalanggardi09@gmail.com>
2025-05-08 11:43:50 +0700

    menambahkan case 5

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -153,33 +153,43 @@ int main()
                  << "Data dengan nomor mahasiswa " << nim << " berhasil dihapus " << endl;
     }
     break;
-    case '3': 
+    case '3':
     {
         mhs.traverse();
     }
     break;
-    case '4': 
+    case '4':
+    {
+        if (mhs.listEmpty() == true)
         {
-            if (mhs.listEmpty() == true)
-            {
-                cout << "\nList Kosong\n";
-                break;
-            }
-            Node *previous, *current;
-            cout << endl
-                 << "Masukkan no mahasiswa yang dicari: ";
-            cin >> nim;
-            if (mhs.Search(nim, &previous, &current) == false)
-                cout << endl
-                     << "Data tidak ditemukan" << endl;
-            else
-            {
-                cout << endl
-                     << "Data ditemukan" << endl;
-                cout << "\nNo Mahasiswa: " << current->noMhs << endl;
-                cout << "\n";
-            }
+            cout << "\nList Kosong\n";
+            break;
         }
-        break;
+        Node *previous, *current;
+        cout << endl
+             << "Masukkan no mahasiswa yang dicari: ";
+        cin >> nim;
+        if (mhs.Search(nim, &previous, &current) == false)
+            cout << endl
+                 << "Data tidak ditemukan" << endl;
+        else
+        {
+            cout << endl
+                 << "Data ditemukan" << endl;
+            cout << "\nNo Mahasiswa: " << current->noMhs << endl;
+            cout << "\n";
+        }
+    }
+    break;
+    case '5': 
+    {
+        exit(0);
+    }
+    break;
+    default:
+    {
+        cout << "Pilihan salah !." << endl;
+    }
+    break;
     }
 }
